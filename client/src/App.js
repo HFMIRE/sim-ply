d375fb324807322540304ba788eeae1db86f090b
HFMIRE <huda.f.mire@gmail.com>
2022-10-22 18:37:38 +0100

    add style

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import Homepage from './components/Homepage';
 import Layout from './components/Layout';
-
+import './style.css';
 function App() {
   return (
     <ChakraProvider theme={theme}>
